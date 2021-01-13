db4ac56b99bbecda3437778078956325cd6ab632
Omid Ahourai <omid.ahourai@coinbase.com>
2021-01-12 21:28:40 -0800

    Bump package version to 1.5.0

--- examples/src/index.js
+++ examples/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import CoinbaseCommerceButton from '../../src';
-import styled, {injectGlobal} from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 
 class App extends React.Component{
   constructor(props){
@@ -25,44 +25,47 @@ class App extends React.Component{
 
   render(){
     return (
-      <div>
-        <span>Enter a checkout ID: </span>
-        <input type='text' onChange={this.updateCheckoutId}/><br/>
-        {this.state.checkoutId.length > 0 ? (
-          <div>
-            <CoinbaseCommerceButton styled={true} checkoutId={this.state.checkoutId} />
-            <CoinbaseCommerceButton styled={true} disabled>Disabled Button</CoinbaseCommerceButton>
-            <CoinbaseCommerceButton checkoutId={this.state.checkoutId}>Ugly Button With Crypto</CoinbaseCommerceButton>
-            <Button checkoutId={this.state.checkoutId}>Pretty Custom Button</Button>
-            <DangerButton checkoutId={'wrongo'}>This Button is Bad</DangerButton>
-            <CoinbaseCommerceButton
-              wrapperStyle={{ width: '100%' }}
-              style={{
-                width: '100%',
-                color: 'green',
-                borderColor: 'green',
-                borderRadius: 4,
-                height: 45,
-                cursor: 'pointer',
-              }}
-              checkoutId={this.state.checkoutId}>
-              Custom Styles Button
-              </CoinbaseCommerceButton>
-          </div>
-        ) : null}
+      <React.Fragment>
+        <div>
+          <span>Enter a checkout ID: </span>
+          <input type='text' onChange={this.updateCheckoutId}/><br/>
+          {this.state.checkoutId.length > 0 ? (
+            <div>
+              <CoinbaseCommerceButton styled={true} checkoutId={this.state.checkoutId} />
+              <CoinbaseCommerceButton styled={true} disabled>Disabled Button</CoinbaseCommerceButton>
+              <CoinbaseCommerceButton checkoutId={this.state.checkoutId}>Ugly Button With Crypto</CoinbaseCommerceButton>
+              <Button checkoutId={this.state.checkoutId}>Pretty Custom Button</Button>
+              <DangerButton checkoutId={'wrongo'}>This Button is Bad</DangerButton>
+              <CoinbaseCommerceButton
+                wrapperStyle={{ width: '100%' }}
+                style={{
+                  width: '100%',
+                  color: 'green',
+                  borderColor: 'green',
+                  borderRadius: 4,
+                  height: 45,
+                  cursor: 'pointer',
+                }}
+                checkoutId={this.state.checkoutId}>
+                Custom Styles Button
+                </CoinbaseCommerceButton>
+            </div>
+          ) : null}
 
-        <span>Enter a charge ID: </span>
-        <input type='text' onChange={this.updateChargeId}/><br/>
-        {this.state.chargeId.length > 0 ? (
-          <div>
-            <CoinbaseCommerceButton styled={true} chargeId={this.state.chargeId}/>
-            <CoinbaseCommerceButton styled={true} disabled>Disabled Button</CoinbaseCommerceButton>
-            <CoinbaseCommerceButton chargeId={this.state.chargeId}>Ugly Button With Crypto</CoinbaseCommerceButton>
-            <Button chargeId={this.state.chargeId}>Pretty Custom Button</Button>
-            <DangerButton chargeId={'wrongo'}>This Button is Bad</DangerButton>
-          </div>
-        ) : null}
-      </div>
+          <span>Enter a charge ID: </span>
+          <input type='text' onChange={this.updateChargeId}/><br/>
+          {this.state.chargeId.length > 0 ? (
+            <div>
+              <CoinbaseCommerceButton styled={true} chargeId={this.state.chargeId}/>
+              <CoinbaseCommerceButton styled={true} disabled>Disabled Button</CoinbaseCommerceButton>
+              <CoinbaseCommerceButton chargeId={this.state.chargeId}>Ugly Button With Crypto</CoinbaseCommerceButton>
+              <Button chargeId={this.state.chargeId}>Pretty Custom Button</Button>
+              <DangerButton chargeId={'wrongo'}>This Button is Bad</DangerButton>
+            </div>
+          ) : null}
+        </div>
+        <GlobalStyle />
+      </React.Fragment>
       )
   }
 }
@@ -89,7 +92,7 @@ const Button = styled(CoinbaseCommerceButton)`
   }
 `;
 
-const DangerButton = Button.extend`
+const DangerButton = styled(Button)`
   background: #FF4136;
   &:hover{
     box-shadow: 7px 7px 0px 0px #00449E;
@@ -100,7 +103,7 @@ const DangerButton = Button.extend`
   }
 `;
 
-injectGlobal`
+const GlobalStyle = createGlobalStyle`
   body {
     background-color: #f4f4f4;
     font-family: system-ui, sans;
